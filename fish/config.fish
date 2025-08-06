6ff30b6e9cb3a4bb88e64d9339089a04c19331b9
Richard Bates <rich.bates@protonmail.com>
2025-08-06 02:41:43 +0100

    feat: remove fastfetch from startup

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,6 @@
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fastfetch
-    echo
-    commandline -f repaint
 end
 
 source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
