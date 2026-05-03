756326bb65a5810dbf55850b01d4e61c7192e6c1
Richard Bates <rich.bates@protonmail.com>
2026-05-03 21:19:35 +0100

    fix: add Homebrew to PATH in config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Homebrew (Apple Silicon)
+if test -d /opt/homebrew
+    fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+end
+
 set -gx CPPFLAGS "-I/Library/Developer/CommandLineTools/SDKs/MacOSX15.sdk/usr/include/c++/v1/ $CPPFLAGS"
 
 starship init fish | source
