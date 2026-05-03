7a38115f05208e519f2fe8b39b18c486bff99170
Richard Bates <rich.bates@protonmail.com>
2026-05-03 22:50:41 +0100

    feat: add omarchy-style theme switching
    Adds a `theme-set` fish function that swaps Ghostty, Alacritty, btop,
    Neovim, and Fish colours in one command. All 19 omarchy themes included
    with backgrounds. Terminal configs now import a generated color file
    rather than hardcoding Tokyo Night.

---
+++ default/themed/fish.theme.tpl
@@ -0,0 +1,36 @@
+# name: generated
+fish_color_normal {{ foreground_strip }}
+fish_color_command {{ color4_strip }}
+fish_color_keyword {{ color5_strip }}
+fish_color_quote {{ color2_strip }}
+fish_color_redirection {{ color6_strip }}
+fish_color_end {{ color6_strip }}
+fish_color_error {{ color1_strip }}
+fish_color_param {{ foreground_strip }}
+fish_color_comment {{ color8_strip }} --italics
+fish_color_match --background={{ color8_strip }}
+fish_color_search_match --background={{ color8_strip }} --bold
+fish_color_selection --background={{ color8_strip }} --bold
+fish_color_operator {{ color6_strip }}
+fish_color_escape {{ color5_strip }}
+fish_color_cwd {{ color6_strip }}
+fish_color_cwd_root {{ color1_strip }}
+fish_color_valid_path --underline
+fish_color_autosuggestion {{ color8_strip }}
+fish_color_user {{ color2_strip }}
+fish_color_host {{ foreground_strip }}
+fish_color_host_remote {{ color3_strip }}
+fish_color_status {{ color1_strip }}
+fish_color_cancel {{ color1_strip }} --reverse
+fish_color_history_current --bold
+fish_pager_color_completion {{ foreground_strip }}
+fish_pager_color_description {{ color3_strip }} --italics
+fish_pager_color_prefix {{ color4_strip }} --bold --underline
+fish_pager_color_progress {{ foreground_strip }} --background={{ color8_strip }}
+fish_pager_color_secondary_completion {{ color8_strip }}
+fish_pager_color_secondary_description {{ color8_strip }}
+fish_pager_color_secondary_prefix {{ color8_strip }}
+fish_pager_color_selected_background --background={{ color8_strip }}
+fish_pager_color_selected_completion {{ foreground_strip }}
+fish_pager_color_selected_description {{ color3_strip }}
+fish_pager_color_selected_prefix {{ color4_strip }}
---
+++ fish/.config/fish/functions/theme-set.fish
@@ -0,0 +1,71 @@
+function theme-set --description "Switch dots.macos theme"
+    set -l DOTS ~/code/dots.macos
+    set -l THEMES_DIR $DOTS/themes
+    set -l TEMPLATES_DIR $DOTS/default/themed
+
+    if test -z "$argv[1]"
+        echo "Usage: theme-set <theme-name>"
+        echo ""
+        echo "Available themes:"
+        for d in $THEMES_DIR/*/
+            echo "  "(basename $d)
+        end
+        return 1
+    end
+
+    set -l THEME $argv[1]
+    set -l THEME_DIR $THEMES_DIR/$THEME
+
+    if not test -d $THEME_DIR
+        echo "theme-set: unknown theme '$THEME'"
+        return 1
+    end
+
+    set -l COLORS_FILE $THEME_DIR/colors.toml
+
+    if not test -f $COLORS_FILE
+        echo "theme-set: no colors.toml for '$THEME'"
+        return 1
+    end
+
+    # Build sed script from colors.toml using awk (reliable quote/whitespace handling)
+    set -l sed_script (mktemp)
+    awk -F'=' '/^[[:space:]]*#/{next} /^[[:space:]]*$/{next} NF>=2{key=$1; gsub(/[[:space:]]/,"",key); value=$0; sub(/^[^=]+=/,"",value); gsub(/^[[:space:]]*/,"",value); gsub(/^"/,"",value); gsub(/"[[:space:]]*$/,"",value); stripped=value; sub(/^#/,"",stripped); printf "s|{{ %s }}|%s|g\n",key,value; printf "s|{{ %s_strip }}|%s|g\n",key,stripped}' $COLORS_FILE > $sed_script
+
+    # Generate ghostty colors
+    mkdir -p ~/.config/ghostty
+    sed -f $sed_script $TEMPLATES_DIR/ghostty.conf.tpl > ~/.config/ghostty/colors.conf
+
+    # Generate alacritty colors
+    mkdir -p ~/.config/alacritty
+    sed -f $sed_script $TEMPLATES_DIR/alacritty.toml.tpl > ~/.config/alacritty/colors.toml
+
+    # Generate btop theme
+    mkdir -p ~/.config/btop/themes
+    sed -f $sed_script $TEMPLATES_DIR/btop.theme.tpl > ~/.config/btop/themes/current.theme
+
+    # Apply fish colours
+    set -l fish_theme_file (mktemp)
+    sed -f $sed_script $TEMPLATES_DIR/fish.theme.tpl > $fish_theme_file
+    while read -l line
+        string match -qr '^\s*#' -- $line; and continue
+        string match -qr '^\s*$' -- $line; and continue
+        set -l varname (string match -r '^\S+' -- $line)
+        set -l value_str (string replace -r '^\S+\s*' '' -- $line)
+        if test -n "$value_str"
+            set -U $varname (string split -- ' ' $value_str)
+        end
+    end < $fish_theme_file
+    rm $fish_theme_file
+
+    # Update neovim colorscheme
+    if test -f $THEME_DIR/neovim.lua
+        cp $THEME_DIR/neovim.lua $DOTS/nvim/.config/nvim/lua/plugins/theme.lua
+    end
+
+    # Save current theme name
+    echo $THEME > ~/.config/dots-theme
+
+    rm $sed_script
+    echo "theme-set: switched to $THEME"
+end
